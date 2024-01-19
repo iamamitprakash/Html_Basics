5100b99ccf8300068023dd44ee9428a8eb71f4e2
Amit Prakash <prakashamit37@gmail.com>
2024-01-19 21:00:41 +0530

    Added Paddle Logic

--- 17.Project_PingPongGame/index.js
+++ 17.Project_PingPongGame/index.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", () =>{
     console.log("Starting game");
     let ball = document.getElementById("ball");
-    let table = document.getElementById("ping-pong-table")
+    let table = document.getElementById("ping-pong-table");
+    let paddle = document.getElementById("paddle");
     let ballX = 100; // x coordinate of the ball
     let ballY = 300; // y coordinate of the ball
 
@@ -16,6 +17,25 @@ document.addEventListener("DOMContentLoaded", () =>{
 
     let dx = 2; // +1 -> it will move the ballX by 1px right and -1 -> moves the ballX by 1px left
     let dy = 2; // +1 -> it will move the ballY by 1px bottom and -1 -> moves the ballY by 1px top
+    let paddleY = 0;
+    paddle.style.top = `${paddleY}px`;
+
+    document.addEventListener("keydown", (event) => {
+        if(event.keyCode == 38 && paddleY > 0){
+            paddleY -= 10;
+        }else if(event.keyCode == 40 && paddleY < table.offsetHeight - paddle.offsetHeight){
+            paddleY += 10;
+        }
+        paddle.style.top = `${paddleY}px`;
+    });
+
+    document.addEventListener("mousemove", (event) =>{
+        // console.log(event.clientY, event.clientX);
+        let mousePoint = event.clientY - table.offsetTop -paddle.offsetHeight/2;
+        paddleY = mousePoint;
+        if(paddleY < 0 || paddleY > table.offsetHeight - paddle.offsetHeight) return;
+        paddle.style.top = `${paddleY}px`;
+    });
 
 
     function gameBall(){
@@ -33,6 +53,11 @@ document.addEventListener("DOMContentLoaded", () =>{
             dx *= -1;
         }
 
+        if(ballX < paddle.offsetLeft + paddle.offsetWidth && ballY > paddleY && ballY + ball.offsetHeight  < paddleY + paddle.offsetHeight){
+            console.log("Collision");
+            dx *= -1;
+        }
+
     }
 
     function loop(){
@@ -41,4 +66,4 @@ document.addEventListener("DOMContentLoaded", () =>{
     }
     loop();
 
-})//2:17
+})
