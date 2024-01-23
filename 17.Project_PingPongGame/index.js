c651ce5a35d910a6b740e4f974ee1085136ec872
Amit Prakash <prakashamit37@gmail.com>
2024-01-23 15:36:00 +0530

    Make the paddle move

--- 17.Project_PingPongGame/index.js
+++ 17.Project_PingPongGame/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let table = document.getElementById("ping-pong-table");
     let ball = document.getElementById("ball");
+    let paddle = document.getElementById("paddle");
 
     let ballX = 50;
     let ballY = 50;
@@ -27,4 +28,15 @@ document.addEventListener("DOMContentLoaded", () => {
         if(ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1;
 
     }, 1);
+
+    let paddleY = 0;
+    let dpY = 5;
+    document.addEventListener("keydown", (event) =>{
+        if(event.keyCode == 38 && paddleY > 0){
+            paddleY += (-1)*dpY;
+        }else if(event.keyCode == 40 && paddleY < table.offsetHeight - paddle.offsetHeight){
+            paddleY += dpY;
+        }
+        paddle.style.top = `${paddleY}px`;
+    })
 });
