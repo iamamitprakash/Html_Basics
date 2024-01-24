fa27d01b649f9307c67121e3e72e062024e8e0dd
Amit Prakash <prakashamit37@gmail.com>
2024-01-24 16:11:24 +0530

    Complete the ping pong game

--- 17.Project_PingPongGame/index.js
+++ 17.Project_PingPongGame/index.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let table = document.getElementById("ping-pong-table");
     let ball = document.getElementById("ball");
     let paddle = document.getElementById("paddle");
+    let gameOverDiv = document.getElementById("game-over");
+    let tryAgainBtn = document.getElementById("try-again-btn");
 
     let ballX = 50;
     let ballY = 50;
@@ -24,10 +26,42 @@ document.addEventListener("DOMContentLoaded", () => {
         // if(ballX > 700-20 || ballX <= 0) dx *= -1;
         // if(ballY > 400-20 || ballY <= 0) dy *= -1;
 
+        // Collision of ball and paddle
+        if(ballX < paddle.offsetLeft + paddle.offsetWidth && 
+            ballY > paddle.offsetTop && 
+            ballY - ball.offsetHeight < paddle.offsetTop+ paddle.offsetHeight){
+                dx *= -1;
+        }
+
         if(ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx *= -1;
         if(ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1;
 
+        // Check if the ball hits the left side of the table
+        if (ballX <= 0) {
+            gameOverDiv.style.display = "block";
+            
+            dx = 0;
+            dy = 0;
+        }
+
     }, 1);
+
+    tryAgainBtn.addEventListener("click", () => {
+        // Hide the game over message
+        gameOverDiv.style.display = "none";
+
+        // Reset ball and paddle positions
+        ballX = 50;
+        ballY = 50;
+        paddleY = 0;
+        ball.style.left = `${ballX}px`;
+        ball.style.top = `${ballY}px`;
+        paddle.style.top = `${paddleY}px`;
+
+        // Restart the ball movement
+        dx = 2;
+        dy = 2;
+    });
 
     let paddleY = 0;
     let dpY = 5;
@@ -37,6 +71,15 @@ document.addEventListener("DOMContentLoaded", () => {
         }else if(event.keyCode == 40 && paddleY < table.offsetHeight - paddle.offsetHeight){
             paddleY += dpY;
         }
+        paddle.style.top = `${paddleY}px`;
+    });
+
+    document.addEventListener("mousemove", (event) => {
+        let mouseDistanceFromTop = event.clientY;
+        let distanceOfTableFromTop = table.offsetTop;
+        let mousePointControl = mouseDistanceFromTop - distanceOfTableFromTop -paddle.offsetHeight/2;
+        paddleY = mousePointControl;
+        if(paddleY <= 0 || paddleY > table.offsetHeight - paddle.offsetHeight) return;
         paddle.style.top = `${paddleY}px`;
     })
 });
